23ebe5244a77f6ccc5dcff6e64889fc070b70fbc
polskikhdv <polskikhdv@icloud.com>
2024-11-24 21:16:42 +0500

    true/fake math/module_4_1

---
+++ fake_math.py
@@ -0,0 +1,7 @@
+def divide(first, second):
+    if second != 0:
+        res = first / second
+        return res
+    else:
+        return 'Ошибка'
+
---
+++ module_4_1.py
@@ -0,0 +1,16 @@
+from fake_math import divide as fake_
+
+from true_math import divide as true_
+
+result1 = fake_(69, 3)
+result2 = fake_(3, 0)
+result3 = true_(49, 7)
+result4 = true_(15, 0)
+
+print(result1)
+
+print(result2)
+
+print(result3)
+
+print(result4)
